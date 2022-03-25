88aa3fd9c96b8b78016e0891ec7f2b8ed1e8c0f6
Trigun Joshi <trigunjoshi@gmail.com>
2022-03-24 23:04:04 -0700

    changes

--- sumSquareDifference.js
+++ sumSquareDifference.js
@@ -1,10 +1,27 @@
 
-let sumSquare = 0;
-let tempSum = 0;
-for (let i = 1; i <= 100; i++) {
-  sumSquare += i * i;
-  tempSum += i;
-}
-let sumSquareDifference = tempSum * tempSum - sumSquare;
 
-console.log(sumSquareDifference);
+
+const sumSquareDifference = () => {
+  let sumSquare = 0;
+  let tempSum = 0;
+  for (let i = 1; i <= 100; i++) {
+    sumSquare += i * i;
+    tempSum += i;
+  }
+  let sumDifference = tempSum * tempSum - sumSquare;
+
+  console.log(sumDifference);
+  //  return sumDifference;
+
+}
+sumSquareDifference();
+// let sumSquare = 0;
+// let tempSum = 0;
+// for (let i = 1; i <= 100; i++) {
+//   sumSquare += i * i;
+//   tempSum += i;
+// }
+// let sumSquareDifference = tempSum * tempSum - sumSquare;
+
+// console.log(sumSquareDifference);
+
